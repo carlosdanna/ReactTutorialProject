8b328df291a81aa6975a783ee27d9dcd6fad225d
Carlos Danna <carlos.danna@gmail.com>
2016-01-21 15:16:55 -0500

    Http Fetch from server

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,13 +1,19 @@
 var React = require('react');
 var ListItem = require('./ListItem.jsx');
-
-var ingredients = [{"id": 1, "text": "ham"},
-                  {"id": 2, "text": "cheese"},
-                  {"id": 3, "text": "potatoes"}];
+var HTTP = require("../services/httpservice");
 
 var List = React.createClass({
+  getInitialState: function(){
+    return {ingredients: []};
+  },
+  componentWillMount: function(){
+    HTTP.get('/ingredients')
+    .then(function(data){
+      this.setState({ingredients: data});
+    }.bind(this));
+  },
   render: function(){
-    var listItem = ingredients.map(function(item){
+    var listItem = this.state.ingredients.map(function(item){
       return (<ListItem key={item.id} ingredient={item.text} />);
     });
     return (<ul>{listItem}</ul>)
