87812d8c7aa90877c6e31025d1e74c672981c8df
Carlos Danna <carlos.danna@gmail.com>
2016-01-21 09:55:08 -0500

    Routes in React

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,5 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var ListManager = require('./components/ListManager.jsx');
+var Routes = require('./Routes.jsx');
 
-ReactDOM.render(<ListManager title="Ingredients" />, document.getElementById("ingredients"));
-ReactDOM.render(<ListManager title="People" />, document.getElementById("people"));
-ReactDOM.render(<ListManager title="Music" headingColor = "#b31217" />, document.getElementById("music"));
+ReactDOM.render(Routes, document.getElementById("main"));
